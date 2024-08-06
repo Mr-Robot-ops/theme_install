8e0025655d7a1adbe347f322c44cb4cb27bd2f8d
Mr-Robot-ops <55334802+Mr-Robot-ops@users.noreply.github.com>
2024-08-07 01:10:13 +0200

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -13,34 +13,34 @@ FISH_CONFIG_DIR="$HOME/.config/fish"
 
 # Entfernen der Oh My Posh-Installation
 echo "Entferne Oh My Posh..."
-rm -rf "$HOME/bin/oh-my-posh"
+sudo rm -rf "$HOME/bin/oh-my-posh"
 
 # Entfernen von Snap und Snap-Paketen
 echo "Entferne Snap und Snap-Pakete..."
-snap remove lolcat
-apt remove -y snapd
+sudo snap remove lolcat
+sudo apt remove -y snapd
 
 # Entfernen von figlet
 echo "Entferne figlet..."
-apt remove -y figlet
+sudo apt remove -y figlet
 
 # Entfernen von lsd
 echo "Entferne lsd..."
-apt remove -y lsd
+sudo apt remove -y lsd
 
 # Entfernen des geklonten Repositories
 if [ -d "$REPO_DIR" ]; then
     echo "Entferne Repository-Verzeichnis..."
-    rm -rf "$REPO_DIR"
+    sudo rm -rf "$REPO_DIR"
 fi
 
 # Entfernen der kopierten Dateien
 echo "Entferne kopierte Dateien..."
-rm -f "$POSH_THEME_DIR/wholespace.omp.json"
-rm -f "$FISH_CONFIG_DIR/config.fish"
+sudo rm -f "$POSH_THEME_DIR/wholespace.omp.json"
+sudo rm -f "$FISH_CONFIG_DIR/config.fish"
 
 # Entfernen der Verzeichnisse, wenn sie leer sind
-rmdir "$POSH_THEME_DIR" 2>/dev/null
-rmdir "$FISH_CONFIG_DIR" 2>/dev/null
+sudo rmdir "$POSH_THEME_DIR" 2>/dev/null
+sudo rmdir "$FISH_CONFIG_DIR" 2>/dev/null
 
 echo "Deinstallation abgeschlossen."
