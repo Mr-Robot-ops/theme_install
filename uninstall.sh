63f84c05d340fc79154ce758775560eff9a2429d
Mr-Robot-ops <55334802+Mr-Robot-ops@users.noreply.github.com>
2024-08-07 20:00:30 +0200

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -20,7 +20,7 @@ chsh -s /bin/bash || { echo "Fehler beim Setzen von bash als Standardshell"; exi
 # Entfernen der Oh My Posh-Installation
 echo "Entferne Oh My Posh..."
 if [ -d "$POSH_BIN_DIR" ]; then
-    sudo rm -rf "$POSH_BIN_DIR/oh-my-posh"
+    rm -rf "$POSH_BIN_DIR/oh-my-posh"
 else
     echo "Oh My Posh ist nicht installiert."
 fi
@@ -28,15 +28,15 @@ fi
 # Entfernen von figlet
 echo "Entferne figlet..."
 if dpkg -l | grep -q figlet; then
-    sudo apt remove -y figlet
+    apt remove -y figlet
 else
     echo "figlet ist nicht installiert."
 fi
 
 # Entfernen von lsd
 echo "Entferne lsd..."
-if snap list | grep -q lsd; then
-    sudo snap remove lsd
+if command -v lsd &> /dev/null; then
+    rm -f /usr/local/bin/lsd || { echo "Fehler beim Entfernen von lsd"; exit 1; }
 else
     echo "lsd ist nicht installiert."
 fi
@@ -44,7 +44,7 @@ fi
 # Entfernen von fish
 echo "Entferne fish..."
 if dpkg -l | grep -q fish; then
-    sudo apt remove -y fish
+    apt remove -y fish
 else
     echo "fish ist nicht installiert."
 fi
@@ -52,7 +52,7 @@ fi
 # Entfernen von lolcat
 echo "Entferne lolcat..."
 if dpkg -l | grep -q lolcat; then
-    sudo apt remove -y lolcat
+    apt remove -y lolcat
 else
     echo "lolcat ist nicht installiert."
 fi
@@ -60,7 +60,7 @@ fi
 # Entfernen des geklonten Repositories
 if [ -d "$REPO_DIR" ]; then
     echo "Entferne Repository-Verzeichnis..."
-    sudo rm -rf "$REPO_DIR"
+    rm -rf "$REPO_DIR"
 else
     echo "Das Repository-Verzeichnis existiert nicht."
 fi
@@ -68,31 +68,31 @@ fi
 # Entfernen der kopierten Dateien
 echo "Entferne kopierte Dateien..."
 if [ -f "$POSH_THEME_DIR/wholespace.omp.json" ]; then
-    sudo rm -f "$POSH_THEME_DIR/wholespace.omp.json"
+    rm -f "$POSH_THEME_DIR/wholespace.omp.json"
 else
     echo "Die Datei $POSH_THEME_DIR/wholespace.omp.json existiert nicht."
 fi
 
 if [ -f "$FISH_CONFIG_DIR/config.fish" ]; then
-    sudo rm -f "$FISH_CONFIG_DIR/config.fish"
+    rm -f "$FISH_CONFIG_DIR/config.fish"
 else
     echo "Die Datei $FISH_CONFIG_DIR/config.fish existiert nicht."
 fi
 
 # Entfernen der Verzeichnisse, wenn sie leer sind
 if [ -d "$POSH_THEME_DIR" ] && [ -z "$(ls -A $POSH_THEME_DIR)" ]; then
-    sudo rmdir "$POSH_THEME_DIR"
+    rmdir "$POSH_THEME_DIR"
 fi
 
 if [ -d "$FISH_CONFIG_DIR" ] && [ -z "$(ls -A $FISH_CONFIG_DIR)" ]; then
-    sudo rmdir "$FISH_CONFIG_DIR"
+    rmdir "$FISH_CONFIG_DIR"
 fi
 
 # Entfernen des Fish-PPA
 echo "Entferne das Fish PPA..."
 if [ -f /etc/apt/sources.list.d/fish-shell-ubuntu-release-3-jammy.list ]; then
-    sudo rm /etc/apt/sources.list.d/fish-shell-ubuntu-release-3-jammy.list
-    sudo apt update
+    rm /etc/apt/sources.list.d/fish-shell-ubuntu-release-3-jammy.list
+    apt update
 else
     echo "Das Fish PPA ist nicht vorhanden."
 fi
